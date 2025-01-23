085c3417f7409b2cf9fbf67f4e22f55437f2a68e
Erli Matos <erlipmatos@outlook.com>
2025-01-23 17:29:01 -0300

    Entrega Desafio 01.04

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-let inputArray = [];
+let listaAmigos = [];
+
 
 function adicionarAmigo() {
     const amigo = document.getElementById('amigo');
-    const inputValue = amigo.value;
+    const addAmigos = amigo.value;
 
-    if (inputValue) {
-        inputArray.push(inputValue);
+    if (addAmigos) {
+        listaAmigos.push(addAmigos);
+        console.log('addamigo');
         amigo.value = ''; // Clear the input field
         displayArray();
-        console.log
+       
     }
 
 }
-/*
-sortearAmigo(){
 
+function sortearAmigo() 
+    {
 
-}
+    let amigoEscolhido = parseInt(Math.random() * listaAmigos.length + 1);
+    let alert(`Seu amigo secreto é " ${listaAmigos[amigoEscolhido]} "`); 
+
+    }
+
 
 /*
 function displayArray() {
     const outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = '<h3>Recorded Inputs:</h3><ul>' + inputArray.map(item => `<li>${item}</li>`).join('') + '</ul>';
-}
-*/
+    outputDiv.innerHTML = '<h3>Recorded Inputs:</h3><ul>' + listaAmigos.map(item => `<li>${item}</li>`).join('') + '</ul>';
+
+for (conta < listaAmigos.length -1 cont++);
+    if (amigo = listaAmigos[conta]);
+        alert(`Seu amigo secreto é " ${listaAmigos[conta]} "`); 
+
+    } */
